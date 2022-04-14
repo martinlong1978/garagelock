d059822ae18dddb5f97fa019c2ba587ca9769716
Martin Long <martin@longhome.co.uk>
2022-04-14 19:22:26 +0100

    Updated config

--- GarageLock.ino
+++ GarageLock.ino
@@ -8,10 +8,12 @@
 #include <PubSubClient.h>
 #include "MCP23S08.h"
 #include "SECRETS.h"
+#include <Update.h>
 
 #define NO_LOCKS 1
 #define SUBJECT "garagelock/feeds/onoff"
 #define STATUSSUBJECT "garagelock/feeds/onoff/state"
+#define UPDATESUBJECT "garagelock/feeds/updates"
 
 //#define CONFIGMODE
 
@@ -76,32 +78,32 @@ void setupOTA()
   ArduinoOTA.setPort(3232);
 
   // Hostname defaults to esp3232-[MAC]
-  ArduinoOTA.setHostname("garagedoor");
+  // ArduinoOTA.setHostname("garagedoor");
 
   Serial.println("Configuring OTA");
   ArduinoOTA
       .onStart([]()
                {
-      String type;
-      if (ArduinoOTA.getCommand() == U_FLASH)
-        type = "sketch";
-      else // U_SPIFFS
-        type = "filesystem";
+            String type;
+            if (ArduinoOTA.getCommand() == U_FLASH)
+              type = "sketch";
+            else // U_SPIFFS
+              type = "filesystem";
 
-      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-      Serial.println("Start updating " + type); })
+            // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+            Serial.println("Start updating " + type); })
       .onEnd([]()
              { Serial.println("\nEnd"); })
       .onProgress([](unsigned int progress, unsigned int total)
                   { Serial.printf("Progress: %u%%\r", (progress / (total / 100))); })
       .onError([](ota_error_t error)
                {
-      Serial.printf("Error[%u]: ", error);
-      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-      else if (error == OTA_END_ERROR) Serial.println("End Failed"); });
+                  Serial.printf("Error[%u]: ", error);
+                  if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+                  else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+                  else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+                  else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+                  else if (error == OTA_END_ERROR) Serial.println("End Failed"); });
 
   ArduinoOTA.begin();
 }
@@ -219,7 +221,7 @@ void checkDebugQueue()
     for (int i = 0; i < NO_LOCKS; i++)
     {
       // Serial.println("Polling");
-      //locks[i].poll();
+      // locks[i].poll();
     }
     Serial.printf("c - Close Pin: %i\n", locks[debuglock].closePin()->read());
     Serial.printf("l - LockLimit Pin: %i\n", locks[debuglock].lockLimitPin()->read());
